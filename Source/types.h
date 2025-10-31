2e6bd571971a6830a1711e404069392f046e2086
Prayag Sridhar <53558950+PRAYAG2000n@users.noreply.github.com>
2025-10-30 23:52:33 -0400

    Reformat types.h for improved readability

--- Source/types.h
+++ Source/types.h
@@ -1,18 +1,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-// Use float for initial development - switch to ap_fixed later for optimization
+
 typedef float fp16_t;
 typedef float fp32_t;
 
-// IMU data structure
+
 struct IMUData {
     fp16_t accel_x, accel_y, accel_z;  // Accelerometer data (m/s²)
     fp16_t gyro_x, gyro_y, gyro_z;     // Gyroscope data (rad/s)
     fp16_t mag_x, mag_y, mag_z;        // Magnetometer data (uT)
 };
 
-// Attitude (Euler angles) structure
+
 struct Attitude {
     fp16_t roll, pitch, yaw;           // Roll, pitch, yaw angles (radians)
 };
@@ -23,3 +23,4 @@ struct ControlOutput {
 };
 
 #endif
+
